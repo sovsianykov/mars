cecd5f492c415655c7f27d083ec47c153eec4230
”sovsianykov” <swebproger@gmail.com>
2021-02-17 19:10:28 +0200

    first

---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {Row,Col} from 'react-bootstrap'
+
+function Home() {
+
+    return (
+        <div>
+            <Row>
+                <Col className="frame col-sm-6"></Col>
+                <Col className="frame col-sm-3"></Col>
+                <Col className="frame col-sm-3"></Col>
+            </Row>
+            <Row>
+                <div className="frame col-sm-2"></div>
+                <div className="frame col-sm-2"></div>
+                <div className="frame col-sm-2"></div>
+                <div className="frame col-sm-2"></div>
+                <div className="frame col-sm-2"></div>
+                <div className="frame col-sm-2"></div>
+            </Row>
+
+
+
+
+        </div>
+
+    )
+
+}
+export default Home
