f3bdc521752aaab05c063f2eb2cd1820689cb4f9
”sovsianykov” <swebproger@gmail.com>
2021-02-19 12:07:47 +0200

    first 3

--- src/components/api.js
+++ src/components/api.js
@@ -4,8 +4,7 @@ export default {
     getData: () =>
       axios({
             'method':'GET',
-            'url':'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=dOh5PZrZhIk0E6uAUeu9TM92CzVuAhHHNvbRpUPf',
-            // 'headers': {
+            'url':'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=dOh5PZrZhIk0E6uAUeu9TM92CzVuAhHHNvbRpUPf&earth_date=2020-3-3&camera=navCAM'
             //     'content-type':'application/octet-stream',
             //     'x-rapidapi-host':'example.com',
             //     'x-rapidapi-key': process.env.RAPIDAPI_KEY
